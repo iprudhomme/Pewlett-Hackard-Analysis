d6cb6b29bfc41adf5c50a260879637e5dc28189a
Ilan Prudhomme <ilan@theprudhommes.com>
2022-02-15 19:50:46 -0800

    Added additional queries and tables

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -58,3 +58,45 @@ SELECT title, COUNT(emp_no) as emp_count
 FROM mentorship_eligibility
 GROUP BY title;	
 
+
+--####### Additional Queries ###### ---- 
+
+-- Retiring Count by Departments
+SELECT dept_name, title, COUNT(ut.emp_no) AS dept_count
+INTO retiring_by_dept 
+FROM unique_titles ut
+JOIN dept_emp de on ut.emp_no = de.emp_no
+JOIN departments d on de.dept_no = d.dept_no 
+WHERE de.to_date = ('9999-01-01') 
+GROUP BY dept_name, title
+ORDER BY 1,2
+
+-- Retiring Count by Departments
+SELECT dept_name, title, COUNT(ut.emp_no) dept_count
+INTO mentorship_by_dept
+FROM mentorship_eligibility ut
+JOIN dept_emp de on ut.emp_no = de.emp_no
+JOIN departments d on de.dept_no = d.dept_no 
+WHERE de.to_date = ('9999-01-01') 
+GROUP BY dept_name, title
+ORDER BY dept_name, title
+
+-- selecting missing employees from the mentorship program
+SELECT 
+	rd.dept_name as dept_name,
+	rd.title as title,
+	md.dept_count as mentor_count,
+	rd.dept_count as retiring_count
+FROM mentorship_by_dept md
+FULL OUTER JOIN retiring_by_dept rd 
+	ON md.dept_name = rd.dept_name AND md.title = rd.title
+WHERE md.dept_count IS null	
+
+
+
+ 
+ 
+ 
+ 
+
+
